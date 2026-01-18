1210b2684eef492640e102be751abe8c994e8774
Checamaro <kirill.sharevich@yandex.ru>
2026-01-18 18:24:44 +0300

    Практика работы с WebSocket протоколом

---
+++ websocket_users_server.py
@@ -0,0 +1,19 @@
+import asyncio
+import websockets
+
+
+async def handler(websocket):
+    message = await websocket.recv()
+    print(f"Получено сообщение от пользователя: {message}")
+
+    for i in range(1, 6):
+        response = f"{i} Сообщение пользователя: {message}"
+        await websocket.send(response)
+
+
+async def main():
+    server = await websockets.serve(handler, "localhost", 8765)
+    await server.wait_closed()
+
+
+asyncio.run(main())
