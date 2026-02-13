e1ca146a1971ce232e40ae3a8c697ae7b89082fc
Checamaro <kirill.sharevich@yandex.ru>
2026-02-13 14:57:46 +0300

    Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,11 @@ import pytest
 from pydantic import BaseModel, EmailStr
 
 from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 # Импортируем запрос и ответ создания пользователя, модель данных пользователя
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
 
 # Модель для агрегации возвращаемых данных фикстурой function_user
@@ -20,6 +22,14 @@ class UserFixture(BaseModel):
     def password(self) -> str:  # Быстрый доступ к password пользователя
         return self.request.password
 
+    @property
+    def authentication_user(self) -> AuthenticationUserSchema:  # Новое свойство для аутентификации
+        """Возвращает модель аутентификации пользователя."""
+        return AuthenticationUserSchema(
+            email=self.request.email,
+            password=self.request.password
+        )
+
 
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
@@ -30,6 +40,9 @@ def authentication_client() -> AuthenticationClient:
 def public_users_client() -> PublicUsersClient:
     return get_public_users_client()
 
+@pytest.fixture
+def private_users_client(function_user: UserFixture) -> PrivateUsersClient:
+    return get_private_users_client(function_user.authentication_user)
 
 # Фикстура для создания пользователя
 @pytest.fixture
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,15 @@
 from http import HTTPStatus
 import pytest
 
+from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 # Импортируем функцию для проверки ответа создания юзера
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
+
 
 @pytest.mark.users
 @pytest.mark.regression
@@ -21,3 +24,17 @@ def test_create_user(public_users_client: PublicUsersClient):
     assert_create_user_response(request, response_data)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
+    response = private_users_client.get_user_me_api()
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
+
+    assert_get_user_response(response_data, function_user.response)
