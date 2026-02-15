f8d6b09b2948f5b932db16573871d179d20bb47e
Checamaro <kirill.sharevich@yandex.ru>
2026-02-15 18:36:22 +0300

    Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,18 +9,29 @@ from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
+@pytest.mark.parametrize(
+    "email_domain",
+    [
+        "mail.ru",
+        "gmail.com",
+        "example.com"
+    ]
+)
+def test_create_user(email_domain: str, public_users_client: PublicUsersClient):
+    email = fake.email(domain=email_domain)
 
-    request = CreateUserRequestSchema()
+    request = CreateUserRequestSchema(
+        email=email,
+        password=fake.password())
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
-    # Используем функцию для проверки ответа создания юзера
     assert_create_user_response(request, response_data)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
