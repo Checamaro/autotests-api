576b5f399df75ac86f6499f3d83ba226095c9eb6
Checamaro <kirill.sharevich@yandex.ru>
2026-02-17 14:31:30 +0300

    Практика написания API автотестов для работы с курсами

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -4,17 +4,41 @@ import pytest
 
 from clients.courses.courses_client import CoursesClient
 from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, GetCoursesQuerySchema, \
-    GetCoursesResponseSchema
+    GetCoursesResponseSchema, CreateCourseResponseSchema, CreateCourseRequestSchema
 from fixtures.courses import CourseFixture
+from fixtures.files import FileFixture
 from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.courses import assert_update_course_response, assert_get_courses_response
+from tools.assertions.courses import assert_update_course_response, assert_get_courses_response, \
+    assert_create_course_response
 from tools.assertions.schema import validate_json_schema
 
 
 @pytest.mark.courses
 @pytest.mark.regression
 class TestCourses:
+    def test_create_course(
+            self,
+            courses_client: CoursesClient,
+            function_user: UserFixture,
+            function_file: FileFixture
+    ):
+        # 1. Формируем запрос на создание курса
+        request = CreateCourseRequestSchema(
+            preview_file_id=function_file.response.file.id,
+            created_by_user_id=function_user.response.user.id
+        )
+        # 2. Отправляем POST-запрос на создание курса
+        response = courses_client.create_course_api(request)
+        # 3. Десериализуем JSON-ответ в Pydantic-модель
+        response_data = CreateCourseResponseSchema.model_validate_json(response.text)
+        # 4. Проверяем статус код ответа
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # 5. Проверяем, что данные в ответе соответствуют запросу
+        assert_create_course_response(request, response_data)
+        # 6. Валидируем JSON-схему ответа
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
     def test_get_courses(
             self,
             courses_client: CoursesClient,
