fbe0d5423fb830477d3602012302f26d8af24d5e
Checamaro <kirill.sharevich@yandex.ru>
2026-02-24 10:59:02 +0300

    Добавление Allure шагов в автотест

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -1,15 +1,12 @@
 from httpx import Response
-
+import allure
 from clients.api_client import APIClient
 from clients.private_http_builder import AuthenticationUserSchema, get_private_http_client
 from clients.courses.courses_schema import (
     GetCoursesQuerySchema,
-    GetCoursesResponseSchema,
-    GetCourseResponseSchema,
     CreateCourseRequestSchema,
     CreateCourseResponseSchema,
-    UpdateCourseRequestSchema,
-    UpdateCourseResponseSchema,
+    UpdateCourseRequestSchema
 )
 
 
@@ -18,6 +15,7 @@ class CoursesClient(APIClient):
     Клиент для работы с /api/v1/courses
     """
 
+    @allure.step("Get courses")
     def get_courses_api(self, query: GetCoursesQuerySchema) -> Response:
         """
         Метод получения списка курсов.
@@ -27,6 +25,7 @@ class CoursesClient(APIClient):
         """
         return self.get("/api/v1/courses", params=query.model_dump(by_alias=True))
 
+    @allure.step("Get course by id {course_id}")
     def get_course_api(self, course_id: str) -> Response:
         """
         Метод получения курса.
@@ -36,6 +35,7 @@ class CoursesClient(APIClient):
         """
         return self.get(f"/api/v1/courses/{course_id}")
 
+    @allure.step("Create course")
     def create_course_api(self, request: CreateCourseRequestSchema) -> Response:
         """
         Метод создания курса.
@@ -46,6 +46,7 @@ class CoursesClient(APIClient):
         """
         return self.post("/api/v1/courses", json=request.model_dump(by_alias=True))
 
+    @allure.step("Update course by id {course_id}")
     def update_course_api(self, course_id: str, request: UpdateCourseRequestSchema) -> Response:
         """
         Метод обновления курса.
@@ -56,6 +57,7 @@ class CoursesClient(APIClient):
         """
         return self.patch(f"/api/v1/courses/{course_id}", json=request.model_dump(by_alias=True))
 
+    @allure.step("Delete course by id {course_id}")
     def delete_course_api(self, course_id: str) -> Response:
         """
         Метод удаления курса.
